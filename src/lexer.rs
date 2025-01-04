c707c64c0c45bdb9a0df9e4c3ae3e1e4e10513c7
Abdulwaarith <abdulwaarithz@gmail.com>
2025-01-04 13:25:19 +0100

    Implement Display trait for TokenKind and update Token structures
    - Added Display trait implementation for TokenKind to enhance readability and debugging. Each token kind now has a specific string representation that simplifies logging and output processes.
    - Updated Token and TokenKind structures with #[derive(PartialEq, Debug)] to support equality checks and better debugging capabilities.
    - The Token struct now includes public fields for kind and literal, allowing for direct access and manipulation in consuming code.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,19 +1,74 @@
+#[allow(dead_code)]
+
 use crate::token::{Token, TokenKind};
 
 // Lexer is a simple lexer that tokenizes the input string into tokens.
 pub struct Lexer {
     input: Vec<char>,
+    position: usize,
+    read_position: usize,
+    ch: char,
 }
 
 impl Lexer {
-    pub fn new(input: &str) -> Self {
-        Self {
+    // Create a new Lexer instance
+    pub fn new(input: &str) -> Lexer {
+        let mut lexer = Lexer {
             input: input.chars().collect(),
-        }
+            position: 0,
+            read_position: 0,
+            ch: '\0',
+        };
+        // Read the first character
+        lexer.read_char();
+        // Return the lexer
+        lexer
     }
 
-    fn next_token(&self) -> Token {
-        todo!();
+    // Read the next character in the input
+    fn read_char(&mut self) {
+        // If we've reached the end of the input, set the character to the null character
+        if self.read_position >= self.input.len() {
+            self.ch = '\0';
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+        // Update the position and read_position
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    // Read the next token in the input
+    fn next_token(&mut self) -> Token {
+        // Create a new token with the current character
+        let token = match self.ch {
+            '=' => Lexer::new_token(TokenKind::Assign, self.ch),
+            '+' => Lexer::new_token(TokenKind::Plus, self.ch),
+            '(' => Lexer::new_token(TokenKind::LeftParen, self.ch),
+            ')' => Lexer::new_token(TokenKind::RightParen, self.ch),
+            '{' => Lexer::new_token(TokenKind::LeftBrace, self.ch),
+            '}' => Lexer::new_token(TokenKind::RightBrace, self.ch),
+            ',' => Lexer::new_token(TokenKind::Comma, self.ch),
+            ';' => Lexer::new_token(TokenKind::Semicolon, self.ch),
+            '\0' => Token {
+                kind: TokenKind::Eof,
+                literal: "".to_string(),
+            },
+            _ => Lexer::new_token(TokenKind::Illegal, self.ch),
+        };
+        // Read the next character
+        self.read_char();
+        // Return the token
+        token
+    }
+
+    // Create a new token
+    fn new_token(kind: TokenKind, ch: char) -> Token {
+        // Create a new token with the given kind and character literal
+        Token {
+            kind,
+            literal: ch.to_string(),
+        }
     }
 }
 
@@ -42,7 +97,7 @@ mod test {
         ];
 
         // Create a new Lexer instance with the test input
-        let lexer = Lexer::new(input);
+        let mut lexer = Lexer::new(input);
 
         // Iterate over the expected tokens and their corresponding literals
         for (index, exp_token) in expected_tests.iter().enumerate() {

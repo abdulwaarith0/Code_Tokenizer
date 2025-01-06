740fb07bc6132387d01e3fb0a81226449c473831
Abdulwaarith <abdulwaarithz@gmail.com>
2025-01-06 13:07:48 +0100

    Refactored lexer implementation for improved performance Added support for custom token patterns and error handling Updated test cases to cover new functionality

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,4 @@
-#[allow(dead_code)]
-
-use crate::token::{Token, TokenKind};
+use crate::token::{lookup_ident, Token, TokenKind};
 
 // Lexer is a simple lexer that tokenizes the input string into tokens.
 pub struct Lexer {
@@ -40,6 +38,8 @@ impl Lexer {
 
     // Read the next token in the input
     fn next_token(&mut self) -> Token {
+        // Skip the whitespace
+        self.skip_whitespace();
         // Create a new token with the current character
         let token = match self.ch {
             '=' => Lexer::new_token(TokenKind::Assign, self.ch),
@@ -54,12 +54,65 @@ impl Lexer {
                 kind: TokenKind::Eof,
                 literal: "".to_string(),
             },
-            _ => Lexer::new_token(TokenKind::Illegal, self.ch),
+            _ => {
+                return if Lexer::is_letter(self.ch) {
+                    let literal = self.read_identifier();
+                    let kind = lookup_ident(literal.clone());
+                    Token { kind, literal }
+                } else if Lexer::is_digit(self.ch) {
+                    let literal = self.read_number();
+                    Token { kind: TokenKind::Int, literal }
+                } else {
+                    Lexer::new_token(TokenKind::Illegal, self.ch)
+                }
+            }
         };
         // Read the next character
         self.read_char();
         // Return the token
         token
+    }
+
+
+    // Skip the whitespace characters
+    fn skip_whitespace(&mut self) {
+        while self.ch.is_ascii_whitespace() {
+            self.read_char();
+        }
+    }
+
+    // Check if the character is a letter
+    fn is_letter(ch: char) -> bool {
+        ch.is_alphabetic() || ch == '_'
+    }
+
+    // Read the identifier from the input
+    fn read_identifier(&mut self) -> String {
+        // Create a new identifier string
+        let mut identifier = String::new();
+        // Read the identifier until the character is not a letter
+        while Lexer::is_letter(self.ch) {
+            identifier.push(self.ch);
+            self.read_char();
+        }
+        identifier
+    }
+
+    // Check if the character is a digit
+    fn is_digit(ch: char) -> bool {
+        ch.is_numeric()
+    }
+
+    // Read the number from the input 
+    fn read_number(&mut self) -> String {
+        // Create a new number string
+        let mut number = String::from("");
+        // Read the number until the character is not a digit
+        while Lexer::is_digit(self.ch) {
+            number.push(self.ch);
+            self.read_char();
+        }
+        number
     }
 
     // Create a new token
@@ -80,18 +133,55 @@ mod test {
     #[test]
     // Test the next_token method of the Lexer
     fn test_next_token() {
-        // Test input string
-        let input = "=+(){},;";
+        // Test input string with multi-line string syntax
+        let input = r#"
+        let five = 5;
+        let ten = 10;
+        
+        let add = fn(x, y) {
+            x + y;
+        };
+        
+        let result = add(five, ten);
+        "#;
 
         // Expected tokens and their corresponding literals
         let expected_tests = vec![
+            (TokenKind::Let, "let".to_string()),
+            (TokenKind::Ident, "five".to_string()),
             (TokenKind::Assign, "=".to_string()),
-            (TokenKind::Plus, "+".to_string()),
+            (TokenKind::Int, "5".to_string()),
+            (TokenKind::Semicolon, ";".to_string()),
+            (TokenKind::Let, "let".to_string()),
+            (TokenKind::Ident, "ten".to_string()),
+            (TokenKind::Assign, "=".to_string()),
+            (TokenKind::Int, "10".to_string()),
+            (TokenKind::Semicolon, ";".to_string()),
+            (TokenKind::Let, "let".to_string()),
+            (TokenKind::Ident, "add".to_string()),
+            (TokenKind::Assign, "=".to_string()),
+            (TokenKind::Function, "fn".to_string()),
             (TokenKind::LeftParen, "(".to_string()),
+            (TokenKind::Ident, "x".to_string()),
+            (TokenKind::Comma, ",".to_string()),
+            (TokenKind::Ident, "y".to_string()),
             (TokenKind::RightParen, ")".to_string()),
             (TokenKind::LeftBrace, "{".to_string()),
+            (TokenKind::Ident, "x".to_string()),
+            (TokenKind::Plus, "+".to_string()),
+            (TokenKind::Ident, "y".to_string()),
+            (TokenKind::Semicolon, ";".to_string()),
             (TokenKind::RightBrace, "}".to_string()),
+            (TokenKind::Semicolon, ";".to_string()),
+            (TokenKind::Let, "let".to_string()),
+            (TokenKind::Ident, "result".to_string()),
+            (TokenKind::Assign, "=".to_string()),
+            (TokenKind::Ident, "add".to_string()),
+            (TokenKind::LeftParen, "(".to_string()),
+            (TokenKind::Ident, "five".to_string()),
             (TokenKind::Comma, ",".to_string()),
+            (TokenKind::Ident, "ten".to_string()),
+            (TokenKind::RightParen, ")".to_string()),
             (TokenKind::Semicolon, ";".to_string()),
             (TokenKind::Eof, "".to_string()),
         ];
